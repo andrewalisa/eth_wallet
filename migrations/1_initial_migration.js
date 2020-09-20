999af64be43ee670dada584d2ea598cdd4f96b88
andrewalisa <andrewkalisa@gmail.com>
2020-09-19 21:16:40 -0400

    Modify the initial_migration.js file to include DaiTokenMock.Change the pattern to ASync/AWait. Lastly, we mint 1000 Dai Tokens for the deployer

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,14 @@
 const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed();
+
+  //Mint 1000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xDb192CDd88E4380Fc3d04EF4bc8436dF56Dc2177',
+    '1000000000000000000000' //1000 + 18 zeros. WebJ3 can help, but following tutorial 
+  )
 };
